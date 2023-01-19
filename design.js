7d41ec579db14268fdc2c551028cb028795b2089
thes5dx <jordanfichter.work@gmail.com>
2023-01-19 09:37:39 -0600

    first commit

---
+++ design.js
@@ -0,0 +1,24 @@
+/* width */
+
+-webkit-scrollbar-width; {
+    width; '(\"10px"\);'
+  }
+  /* Track */
+
+-webkit-scrollbar-track; {
+    '("\#f1f1f1;"\)' 
+  }
+  /* Handle */
+
+-webkit-scrollbar-thumb; {
+    background-color; '(\"#888888;"\)' 
+  }
+
+  /* Handle on hover */
+  
+-webkit-scrollbar-thumb;'("\hover;"\)' ;
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      '(\"#555;"\)';
+    } 
+  
